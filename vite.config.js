1e1ae01ae52e6dbbb8eb972f00172018ba90cca2
hyperruss <gribunovio00049@gmail.com>
2026-05-18 20:34:16 +0300

    деплой и выкладка сайта

--- vite.config.js
+++ vite.config.js
@@ -10,3 +10,15 @@ export default defineConfig({
     port: 4173
   }
 });
+
+export default defineConfig({
+  base: '/frontend-kurs2026/',
+  server: {
+    host: '127.0.0.1',
+    port: 5173
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 4173
+  }
+});
